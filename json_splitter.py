193ca1f30d757a1cf5f5ee6ef0111bcc04b77e5f
Isaac Alvim <ipfalvim@gmail.com>
2022-03-29 13:10:58 -0300

    duplicate remover and onluy duplicates

--- json_splitter.py
+++ json_splitter.py
@@ -9,12 +9,23 @@ def sort_remove_duplicates(file):
     for i in range(len(file)-1):
         if file[i]["student_name"] == file[i+1]["student_name"]:
             removal.append(i+1)
-    rich.print (f"\nForam encontrados {len(removal)} nomes repetidos. Eles já foram removidos :smiley:!")
+    rich.print (f"\n>>> Foram encontrados {len(removal)} nomes repetidos. Eles já foram removidos :smiley:!")
     for item in sorted(removal, reverse=True):
         del file[item]
+    rich.print(f">>> Total final {len(file)} nomes!")
 
-if len(sys.argv) < 4:
-    raise ValueError("Número de argumentos menor que 4")
+def sort_only_duplicates(file):
+    file.sort(key=lambda k: k["student_name"])
+    final = []
+    for i in range(len(file)-1):
+        if file[i]["student_name"] == file[i+1]["student_name"]:
+            final.append(i+1)
+    rich.print (f"\n>>> Foram encontrados {len(final)} nomes repetidos. Eles já foram mergeados :smiley:!")
+    file = final.copy()
+
+
+if len(sys.argv) < 3:
+    raise ValueError("Número de argumentos menor que 3")
     exit()
 
 try:
@@ -26,7 +37,8 @@ except FileNotFoundError:
 
 nomes = sys.argv[2:]
 num_parts = len(nomes)
-sort_remove_duplicates(file)
+# sort_remove_duplicates(file)
+sort_only_duplicates(file)
 split_list = np.array_split(file, num_parts)
 
 for index in range(num_parts):
